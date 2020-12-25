5bd18039a10088e37d6e4a20f1d5c12c53d3bf2b
iamqdk <iamqdk@gmail.com>
2020-12-25 23:56:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -69,6 +69,18 @@
     <script defer src="js/jquery.flexslider.js"></script>
 
     <script type="text/javascript" src='js/jquery.elevatezoom.js'></script></script>
-  
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fe61975a8a254155ab65027/1eqdd6sui';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
    </body>
 </html>
