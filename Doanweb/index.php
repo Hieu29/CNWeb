c5eef6188bfde43ef1aee206c84a092f2e29710b
iamqdk <iamqdk@gmail.com>
2021-01-01 23:37:49 +0700

    Update index.php

--- Doanweb/index.php
+++ Doanweb/index.php
@@ -35,3 +35,16 @@
     error_reporting(0);
     ob_end_flush();
 ?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fe61975a8a254155ab65027/1eqdd6sui';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
